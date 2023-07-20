bf7f3b3df1973184390e29ad6bf59c7e3659bbba
kll1she <kleshewnikm@yandex.ru>
2023-07-20 16:10:04 +0600

    Сделал блок с приветствием

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<app-hello></app-hello>`,
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+}
